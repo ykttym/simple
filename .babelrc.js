cf10596df8bb2a232602b911d052a504a0c00d9b
ykttym <ykttym@126.com>
2020-07-01 03:20:02 +0800

    feat: add client build dep

---
+++ .babelrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "presets": [
+    [
+      "@babel/preset-env",
+      {
+        "targets": {
+          "chrome": 67,
+        },
+        "useBuiltIns": "usage"
+      }
+    ],
+    "@babel/preset-react"
+  ]
+}
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './client.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      }
+    ]
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'public/index.html' },
+      ],
+    }),
+  ],
+};
